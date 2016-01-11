d5a43bd2c79eecb3be323c623e90987ea2debe30
Sergey Dobrynin <sergey.dobrynin@sap.com>
2016-01-11 14:52:29 -0500

    MyTry
    Change-Id: Iec1e543ee1f28caf8d3a510bc8c32cca090de70f

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
-var app = module.exports = require("koa")();
+const Koa = require('koa');
+const app = module.exports = new Koa();
+const convert = require('koa-convert');
 
-var html = 
-    `<html>
-        <head>
-            <meta charset="UTF-8">
-            <title>A small test page</title>
-            <script src="//localhost:9091"></script>
-        </head>
-        <body>
-            Page loaded at: <span id="demo"></span>
-        </body>
-        <script language="javascript">
-                document.getElementById('demo').innerHTML = new Date();
-        </script>
-    </html>`;
-
-app.use(function *(){
-	if(this.request.path === "/client"){
-		this.body = html;
-		return;
-	}
-	this.body = "Koa says Hi!";
+//app.use(convert(function *(){
+//	this.body = "Koa says Hi!";
+//}));
+//
+app.use(ctx => {
+  ctx.body = "Koa says Hi!";
 });
 
 var port = process.env.PORT || (process.argv[2] || 3000);
-port = (typeof port != "number") ? port : 3000;
- 
-if(!module.parent){ app.listen(port); }
 
+if(!module.parent) {
+   app.listen(port);
+}
+console.log("Hello!!!");
 console.log("Application started. Listening on port:" + port);
